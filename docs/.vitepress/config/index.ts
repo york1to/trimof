aa246b059e3aa03fbfe065afd904e4a22b7d678b
Yaozhong Kang <12110225@mail.sustech.edu.cn>
2023-08-31 02:26:34 +0800

    update

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vitepress";
 import themeConfig from "./theme";
 import head from "./head";
-import { DESCRIPTION as description, LANG as lang, TITLE as title } from "./info";
+import { DESCRIPTION as description, TITLE as title } from "./info";
 import markdownItKatex from 'markdown-it-katex'
 import { SitemapStream } from 'sitemap'
 import { createWriteStream } from 'node:fs'
@@ -13,7 +13,6 @@ export default defineConfig({
   description,
   head,
   ignoreDeadLinks: true, // TODO: remove this line when all links are fixed
-  lang,
   lastUpdated: true,
   markdown: {
     config: (md) => {
