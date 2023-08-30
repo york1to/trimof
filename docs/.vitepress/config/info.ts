53657f8c822794c7821150f28fce11a5787b1a09
Yaozhong Kang <12110225@mail.sustech.edu.cn>
2023-08-30 16:45:21 +0800

    update

--- docs/.vitepress/config/info.ts
+++ docs/.vitepress/config/info.ts
@@ -11,7 +11,7 @@ const keywords = [
 // Base Info
 export const TITLE = "trimof";
 export const DESCRIPTION = "york1to's website";
-export const LANG = "en-US";
+export const LANG = "zh-CN";
 export const DOMAIN = "trimof.com";
 export const URL = `https://${DOMAIN}`;
 export const REPO = "https://github.com/yorkitokang/trimof";
@@ -20,9 +20,9 @@ export const KEYWORDS = keywords.join(",");
 export const META_TITLE = `${TITLE}`;
 
 // Algolia Search
-// export const API_KEY = "6e28f201f03a0aea8fcbf38290fd0149";
-// export const APP_ID = "01LBMXU8V5";
-// export const INDEX_NAME = "trimof";
-export const API_KEY = "a8013e96ce5afc3bdea9699964e600b6";
-export const APP_ID = "PATI20OZQ0";
-export const INDEX_NAME = "arch-icekylin";
+export const API_KEY = "6e28f201f03a0aea8fcbf38290fd0149";
+export const APP_ID = "01LBMXU8V5";
+export const INDEX_NAME = "trimof";
+// export const API_KEY = "a8013e96ce5afc3bdea9699964e600b6";
+// export const APP_ID = "PATI20OZQ0";
+// export const INDEX_NAME = "arch-icekylin";
