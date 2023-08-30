2f77e3b7d31ad38cfa44540c78c5ddce43b7f923
Yaozhong Kang <12110225@mail.sustech.edu.cn>
2023-08-30 17:45:11 +0800

    update sitemap

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -3,7 +3,11 @@ import themeConfig from "./theme";
 import head from "./head";
 import { DESCRIPTION as description, LANG as lang, TITLE as title } from "./info";
 import markdownItKatex from 'markdown-it-katex'
+import { SitemapStream } from 'sitemap'
+import { createWriteStream } from 'node:fs'
+import { resolve } from 'node:path'
 
+const links = []
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
   description,
@@ -17,6 +21,16 @@ export default defineConfig({
     }
   },
   themeConfig,
-  title
+  title,
+  buildEnd: async ({ outDir }) => {
+    const sitemap = new SitemapStream({
+      hostname: 'https://trimof.com/'
+    })
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'))
+    sitemap.pipe(writeStream)
+    links.forEach((link) => sitemap.write(link))
+    sitemap.end()
+    await new Promise((r) => writeStream.on('finish', r))
+  }
 });
 
