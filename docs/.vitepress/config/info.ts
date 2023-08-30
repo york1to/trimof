2944ea7a21effbbbb41fb691272e997e879c5d63
Yaozhong Kang <12110225@mail.sustech.edu.cn>
2023-08-30 17:16:48 +0800

    update

--- docs/.vitepress/config/info.ts
+++ docs/.vitepress/config/info.ts
@@ -11,7 +11,7 @@ const keywords = [
 // Base Info
 export const TITLE = "trimof";
 export const DESCRIPTION = "york1to's website";
-export const LANG = "zh-CN";
+export const LANG = "en-US";
 export const DOMAIN = "trimof.com";
 export const URL = `https://${DOMAIN}`;
 export const REPO = "https://github.com/yorkitokang/trimof";
@@ -23,6 +23,3 @@ export const META_TITLE = `${TITLE}`;
 export const API_KEY = "6e28f201f03a0aea8fcbf38290fd0149";
 export const APP_ID = "01LBMXU8V5";
 export const INDEX_NAME = "trimof";
-// export const API_KEY = "a8013e96ce5afc3bdea9699964e600b6";
-// export const APP_ID = "PATI20OZQ0";
-// export const INDEX_NAME = "arch-icekylin";
